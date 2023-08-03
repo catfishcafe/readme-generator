00abaeb75e7edbbc1d0747523708b087bd88e9ee
catfishcafe <akmhobbs@gmail.com>
2023-08-03 16:19:15 -0400

    first step written out in index.js

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
-// TODO: Include packages needed for this application
+//❌ If a line has '❌' it means 'delete this comment before final version'
 
-// TODO: Create an array of questions for user input
+//❌ TODO: Include packages needed for this application
+//❌ Use the 'require' function
+//❌from assignment: 
+  //❌'Your task is to create a command-line application that dynamically generates a professional README.md file from a user's input using the Inquirer packageLinks to an external site'
+//❌so "Inquirer" will be a package to use "require" on (also it's already in the dependencies in package.json so...yeah)
+
+//❌ TODO: Create an array of questions for user input
 const questions = [];
 
-// TODO: Create a function to write README file
+//❌ TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
+//❌ TODO: Create a function to initialize app
 function init() {}
 
 // Function call to initialize app
