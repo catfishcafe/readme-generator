a01914299a2b4a5072585c2aa9edaeee0c8a015a
catfishcafe <akmhobbs@gmail.com>
2023-08-09 22:19:44 -0400

    added more licenses

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const generateMarkdown = require('./utils/generateMarkdown')
   //✅ WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
   //✅ THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
   // WHEN I choose a license for my application from a list of options
-  // THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
+  // THEN a badge for that license is added near the top of the README ✅and a notice is added to the section of the README entitled License that explains which license the application is covered under
   // WHEN I enter my GitHub username
   // THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
   // WHEN I enter my email address
@@ -51,17 +51,11 @@ inquirer
       message: 'Test instructions:',
       name: 'testing',
     },
-    //❌ WHEN I choose a license for my application from a list of options
-  // THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
-    // License: according to the instructions, I have to be able to choose this from a list of options; so the type should be "list" instead of "input." (which I've just learned is the default so...not sure if I needed all of these. maybe it helps to clarify.) anyway let's see if I can make that work:
-    //❌irrelevant but I truly hate trying to spell 'license'
     {
       type: 'list',
       message: 'Choose a license:',
       name: 'license',
-      //also needs 'choices' method so user can...choose
-      //I've only ever used MIT, google says GPL is also popular, and according to instructions I need a 'none' option
-      choices: ['MIT', 'GPL', 'none']
+      choices: ['MIT', 'ISC', 'Zlib', 'none']
     },
     {
       type: 'input',
