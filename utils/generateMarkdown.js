374c33950f73779b7ac8681300201eedabb85bf7
catfishcafe <akmhobbs@gmail.com>
2023-09-01 17:27:25 -0400

    cleaning up more comments

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,4 +1,4 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
+// A function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
   if (license === 'none') {
@@ -7,9 +7,8 @@ function renderLicenseBadge(license) {
     return '![License: ' + license + '](https://img.shields.io/badge/License-' + license + '-yellow.svg)'
   }
 }
-//❌I don't see a GPL badge so I'll test this with MIT and pick a different third option :/
 
-// TODO: Create a function that returns the license link
+// A function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
   if (license === 'none') {
@@ -20,7 +19,7 @@ function renderLicenseLink(license) {
   }
 }
 
-// TODO: Create a function that returns the license section of README
+// A function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
   if (license === 'none') {
@@ -31,7 +30,7 @@ function renderLicenseSection(license) {
   }
 }
 
-// TODO: Create a function to generate markdown for README
+// A function to generate markdown for README
 function generateMarkdown(data) {
   return `
   # ${data.title}
